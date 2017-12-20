61b943846df1451284ff1a9694eb00c23dc56fca
AWebZen <30926788+AWebZen@users.noreply.github.com>
2017-12-20 14:05:27 +0100

    Update split.sh

--- split.sh
+++ split.sh
@@ -1,7 +1,10 @@
 #Prend un fichier assembly en argument et telecharge les genomes complets.
-echo "L'argument que vous avez entre est : "$1
+if [[ $# -eq 0 ]] ; then
+    echo 'No arguments given. Needs an assembly file.'
+    exit 1
+fi
+
 awk -F"\t" ' $12=="Complete Genome"{print $20}' $1 >tmp.txt #Adresses web des dossiers des especes d'archaea a genome complet
-#sed 's/[/]GCA_.*$/_genomic.fna.gz/g' tmp.txt 
 sed 's/\(GCA_.*\)$/\1\/\1_genomic.fna.gz/g' tmp.txt > tmp2.txt
 rm tmp.txt
 while IFS='' read -r line || [[ -n "$line" ]]; do
